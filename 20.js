25409a7431b0f5f52b76a2bd6dace75dd15ec739
arianapo <arianasoledad.tec@gmail.com>
2025-05-15 21:46:27 -0300

    Add files via upload

---
+++ 20.js
@@ -0,0 +1,11 @@
+function resta(x, y) {
+
+    return x - y;
+    console.log(resta(x, y)); // 0
+    x=5;
+    y=8;
+    console.log(resta(x, y)); // -3
+    console.log(resta(5, 5)); // 0
+    
+  }
+  
